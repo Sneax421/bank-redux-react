8f9e953f49f515fe8bc08ad0d9db98673493fc5e
Sneax421 <curilov.german@web.de>
2025-04-04 12:40:20 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+import './App.css'
+import Balance from "./components/Balance.jsx";
+import Operation from "./components/Operation.jsx";
+
+function App() {
+  const [balance, setBalance] = useState(0);
+
+  const deposit = sum => setBalance(prevBalance => prevBalance + sum);
+
+  const withdraw = sum => setBalance(prevBalance => prevBalance < sum ? prevBalance : prevBalance - sum);
+
+  return (
+    <div className="App">
+        <Balance balance={balance} />
+        <Operation deposit={deposit} withdraw={withdraw} />
+    </div>
+  )
+}
+
+export default App
