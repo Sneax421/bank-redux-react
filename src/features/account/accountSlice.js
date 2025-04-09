1374ee91d400268375fa462a08cc3aab197a8683
Sneax421 <curilov.german@web.de>
2025-04-09 10:08:07 +0200

    rtk 2

--- src/features/account/accountSlice.js
+++ src/features/account/accountSlice.js
@@ -6,11 +6,8 @@ const accountSlice = createSlice({
     name: "account",
     initialState: 0,
     reducers: {
-        deposit: (state, action) =>
-            state += action.payload,
-
-        withdraw: (state, action) =>
-           state < action.payload ? state : state - action.payload
+        deposit: (state, action) => state += action.payload,
+        withdraw: (state, action) => state < action.payload ? state : state - action.payload
     }
 })
 
